a280b53670d8662bc27386fbac8335b78c32e1d9
hrishi khawas <hrishikhawas1234@gmail.com>
2026-06-03 17:55:32 +0530

    Modularization

--- rockpaper.py
+++ rockpaper.py
@@ -1,35 +1,38 @@
-
-while True:
-    x=input("wanna play rock paper scissors").lower()
-    if x == "yes":
-
-        import random 
-
+import random 
+def userInput():
+                return input("input r for rock 'p' for paper and 's' for scissors   ").lower()
+def whowon( y):   
         wins = {
-            'r':'s',
-            'p':'r',
-            's':'p'
-        }
-
+                'r':'s',
+                'p':'r',
+                's':'p'}
         emoji= {
-            "r": '🪨',
-            'p':'📃',
-            's':'✂️'
-        }
-
-        y = input("input r for rock 'p' for paper and 's' for scissors   ").lower()
+                "r": '🪨',
+                'p':'📃',
+                's':'✂️'
+            }
         z = random.choice(['r','s','p'])
 
         if y not in ['r','s','p']:
-            print("invalid input")
+                    print("invalid input")
         elif z == y:
-            print("draw")
+                    print("draw")
         elif wins[y] == z:
-            print(f'u win u chose {y} {emoji[y]}   and the computer chose {z} "{emoji[z]}')
-            break;
+                    print(f'u win u chose {y} {emoji[y]}   and the computer chose {z} "{emoji[z]}')
+            
         else:
-            print(f"u lose you chose {y}  {emoji[y]} and the computer chose {z} {emoji[z]}")
+                    print(f"u lose you chose {y}  {emoji[y]} and the computer chose {z} {emoji[z]}")
+def main():
+    x=input("wanna play rock paper scissors\t").lower()
+    if x == "yes":
+        y=userInput()
+        whowon(y)
+
+
+            
     elif x =="no":
-        print("oky ")
+            print("oky ")
     else :
-        print("invalid input")
+            print("invalid input")
+
+main()
